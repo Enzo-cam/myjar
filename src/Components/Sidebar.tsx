4819031140e87c3edb8a6e4e80fe5a4b6ce2d6ea
EnzoCam7 <camerac09@gmail.com>
2023-09-17 21:26:12 -0300

    add: implementation into sidebar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,14 +1,21 @@
+import { useContext } from "react"
 import { Box, Drawer, List, ListItem, Divider, ListItemIcon, ListItemText, Typography } from "@mui/material"
 import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined'
 import MailOutlinedIcon from '@mui/icons-material/MailOutlineOutlined'
+import JarContext from "@/Context/UIContext"
+
 
 const menuItems : string[] = ['Inbox', 'Starred', 'Send email', 'Drafts']
 
 const Sidebar = () => {
+
+    const { sidebarMenuOpen, closeSideMenu } = useContext(JarContext)
+
     return (
         <Drawer
             anchor="left"
-            open={false}
+            open={ sidebarMenuOpen }
+            onClose={closeSideMenu}
         >   
             <Box sx={{width: 250}}>
                 <Box sx={{padding: '5px 10px'}}>
