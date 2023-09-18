e6e51fff604c8899c4e4e05f566f9fba729f812f
EnzoCam7 <camerac09@gmail.com>
2023-09-17 21:26:40 -0300

    Add: JarProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider, CssBaseline } from '@mui/material'
 import { lightTheme } from '@/themes'
+import { UIProvider } from '@/Context/UIProvider'
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UIProvider>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
   ) 
 }
