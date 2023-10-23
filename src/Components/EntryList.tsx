8c34edbd52d5baecd19979f48735fd342276bb3e
EnzoCam7 <camerac09@gmail.com>
2023-10-23 20:24:51 -0300

    feat: add entry card and list component

---
+++ src/Components/EntryList.tsx
@@ -0,0 +1,14 @@
+import {Paper, List} from '@mui/material'
+
+export const EntryList = () => {
+    return (
+        <div>
+            <Paper sx={{height: 'calc(100vh - 250px)', backgroundColor: 'transparent'}}>
+                <List sx={{opacity: 1}}>
+                    <h1>Itemmmm </h1>
+                </List>
+
+            </Paper>
+        </div>
+    )
+}
