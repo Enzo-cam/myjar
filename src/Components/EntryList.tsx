b003b00b1b31bd085bf2ae67c1acfac9d5eb872f
EnzoCam7 <camerac09@gmail.com>
2023-10-24 18:26:43 -0300

    feat: entries's list and data

--- src/Components/EntryList.tsx
+++ src/Components/EntryList.tsx
@@ -1,11 +1,33 @@
+import {FC, useContext, useMemo} from 'react'
+import { EntriesContext } from '@/Context/Entries'
 import {Paper, List} from '@mui/material'
+import EntryCard from './EntryCard'
+import { EntryStatus } from '@/Interfaces'
 
-export const EntryList = () => {
+interface Props{
+    status: EntryStatus;
+}
+
+
+export const EntryList:FC<Props> = ({status}) => {
+
+    const {entries} = useContext(EntriesContext)
+
+    const entriesByStatus = useMemo(() => entries.filter(entry => entry.status === status), [entries])
+    
+    
+
+
     return (
+        // TODO: Aqui se da el drop
         <div>
-            <Paper sx={{height: 'calc(100vh - 250px)', backgroundColor: 'transparent'}}>
+            <Paper sx={{height: 'calc(100vh - 180px)', backgroundColor: 'transparent', padding: '1px 5px' }}>
                 <List sx={{opacity: 1}}>
-                    <h1>Itemmmm </h1>
+                    {
+                        entriesByStatus.map(entry => (
+                            <EntryCard key={entry._id} entry={entry} />
+                        ))
+                    }
                 </List>
 
             </Paper>
